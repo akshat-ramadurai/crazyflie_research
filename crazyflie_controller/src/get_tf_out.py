52bd4d02c7c281f4f19224cbcfc3848118a73c80
Akshat <akshatramadurai@utexas.edu>
2019-04-25 19:43:01 -0500

    Added a listener to subscribe to topics

--- crazyflie_controller/src/get_tf_out.py
+++ crazyflie_controller/src/get_tf_out.py
@@ -4,9 +4,11 @@ import tf
 from geometry_msgs.msg import TransformStamped
 if __name__ == '__main__':
 	rospy.init_node('get_tf_out')
+	
 	listener = tf.TransformListener()
 	rate = rospy.Rate(30.0)
 	while not rospy.is_shutdown():
+		print('It\'s working')
 		try:
 			(trans,rot) = listener.lookupTransform('/world', '/vicon/crazyflie2/crazyflie2', rospy.Time(0))
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
@@ -14,5 +16,7 @@ if __name__ == '__main__':
 			f = open('~/crazyflie_ws/pos.txt','a')
 			f.write('%.6f %.6f %.6f\n' % (trans[0],trans[1],trans[2]))
 			f.close()
-			print('It\'s working')
+			
+
+
 		rate.sleep()
